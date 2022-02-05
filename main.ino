48b5fbe7e689894104c5ed85dc424794c879450e
Giovanni Leon <giovanni.r.leon@gmail.com>
2022-02-04 20:59:35 -0700

    added other two ADS1115s

--- main.ino
+++ main.ino
@@ -2,8 +2,13 @@
 
 Adafruit_ADS1115 ads0;
 Adafruit_ADS1115 ads1;
+Adafruit_ADS1115 ads2;
+Adafruit_ADS1115 ads3;
 
+//the ads.begin() with no parameters defaults to 0b1001000
 #define ADDR_1 (0b1001001)
+#define ADDR_2 (0b1001010)
+#define ADDR_3 (0b1001011)
 
 void setup(void)
 {
@@ -26,13 +31,25 @@ void setup(void)
     // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
     // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
 
+    if (!ads3.begin(ADDR_3))
+    {
+        Serial.println("Failed to initialize ADS Board #3.");
+        while (1)
+            ;
+    }
+
+    if (!ads2.begin(ADDR_2))
+    {
+        Serial.println("Failed to initialize ADS Board #2.");
+        while (1)
+            ;
+    }
     if (!ads1.begin(ADDR_1))
     {
         Serial.println("Failed to initialize ADS Board #1.");
         while (1)
             ;
     }
-
     if (!ads0.begin())
     {
         Serial.println("Failed to initialize ADS Board #0.");
@@ -56,7 +73,7 @@ void loop(void)
 
         Serial.print("A0:");
         Serial.print(volts);
-        Serial.print("v            ");
+        Serial.print("v     ");
 
         adc_number = 1;
     }
@@ -67,6 +84,26 @@ void loop(void)
         volts = ads1.computeVolts(adc);
 
         Serial.print("A1:");
+        Serial.print(volts);
+        Serial.print("v     ");
+        adc_number = 2;
+    }
+    else if (adc_number == 2)
+    {
+        adc = ads2.readADC_SingleEnded(0);
+        volts = ads2.computeVolts(adc);
+
+        Serial.print("A2:");
+        Serial.print(volts);
+        Serial.print("v     ");
+        adc_number = 3;
+    }
+    else if (adc_number == 3)
+    {
+        adc = ads3.readADC_SingleEnded(0);
+        volts = ads3.computeVolts(adc);
+
+        Serial.print("A3:");
         Serial.print(volts);
         Serial.println("v");
 
