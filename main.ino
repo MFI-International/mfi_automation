ee4e461b556f429c7716ded94810fd0ed94ec592
Giovanni Leon <giovanni.r.leon@gmail.com>
2022-03-24 04:23:13 -0600

    Added basic server client commands, reduced ADC board to 1

--- main.ino
+++ main.ino
@@ -1,42 +1,86 @@
 #include <Adafruit_ADS1X15.h>
-
 #include <SPI.h>
+
 #include <Ethernet.h>
+#include <EthernetUdp.h>
+
+#include <string.h>
+#include <math.h>
 
 Adafruit_ADS1115 ads0;
-Adafruit_ADS1115 ads1;
-Adafruit_ADS1115 ads2;
-Adafruit_ADS1115 ads3;
 
-#define ADDR_1 (0b1001001)
-#define ADDR_2 (0b1001010)
-#define ADDR_3 (0b1001011)
-
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {
-    0xA8, 0x61, 0x0A, 0xAE, 0x75, 0x2C};
+byte mac[] = {0xA8, 0x61, 0x0A, 0xAE, 0x75, 0x2C};
 IPAddress ip(192, 168, 1, 177);
 
-int8_t adc_number = 0;
-double adcVals[16] = {1.1, 2, 1.5, 3.1, 4, 5, 6, 1.6, 8.4, 1.1, 2, 1, 3.1, 4, 5, 6};
+unsigned int localPort = 8888; // local port to listen on
 
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// (port 80 is default for HTTP):
-EthernetServer server(80);
+bool autoBroadcasting = 1;
+double adcLevel; // out of 5v
+int transitionLevels = 5;
 
+// buffers for receiving and sending data
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; // buffer to hold incoming packet,
+char ReplyBuffer[] = "acknowledged";       // a string to send back
+
+// An EthernetUDP instance to let us send and receive packets over UDP
+EthernetUDP Udp;
+
+// void udpWrite(*char cmd, *char val)
+// {
+// }
+
+void sendUPDString(char *string)
+{
+    Udp.beginPacket(Udp.remoteIP(), 5555);
+    Udp.write(string);
+    Udp.endPacket();
+}
+
+int getCommandVaue(char *command)
+{
+    // value is the total UDP packet size (24 bytes) minus the command size (4 bytes, "XXX:") at command index 4
+    char val[20];
+    strncpy(val, &command[4], 20);
+    return atoi(val);
+}
+
+bool commandValValid(int begin, int end, char *command)
+{
+    Serial.println(getCommandVaue(command));
+    if (command[4] == ' ' || command[4] == 13)
+    {
+        return 0;
+    }
+
+    if (getCommandVaue(command) > end || getCommandVaue(command) < begin)
+    {
+        return 0;
+    }
+    return 1;
+}
+
+char *getCommandType(char *command)
+{
+    char type[4];
+    strncpy(type, &command[0], 3);
+    type[3] = '\0';
+    // Serial.println(type);
+    return type;
+}
 void setup()
 {
+
+    // start the Ethernet
+    Ethernet.begin(mac, ip);
+
+    // Open serial communications and wait for port to open:
     Serial.begin(9600);
     while (!Serial)
     {
         ; // wait for serial port to connect. Needed for native USB port only
     }
-    Serial.println("Ethernet WebServer Example");
 
-    // start the Ethernet connection and the server:
-    Ethernet.begin(mac, ip);
+    Serial.println("Serial Enabled with baud at 9600");
 
     // Check for Ethernet hardware present
     if (Ethernet.hardwareStatus() == EthernetNoHardware)
@@ -52,30 +96,6 @@ void setup()
         Serial.println("Ethernet cable is not connected.");
     }
 
-    // start the server
-    server.begin();
-    Serial.print("server is at ");
-    Serial.println(Ethernet.localIP());
-
-    if (!ads3.begin(ADDR_3))
-    {
-        Serial.println("Failed to initialize ADS Board #3.");
-        while (1)
-            ;
-    }
-
-    if (!ads2.begin(ADDR_2))
-    {
-        Serial.println("Failed to initialize ADS Board #2.");
-        while (1)
-            ;
-    }
-    if (!ads1.begin(ADDR_1))
-    {
-        Serial.println("Failed to initialize ADS Board #1.");
-        while (1)
-            ;
-    }
     if (!ads0.begin())
     {
         Serial.println("Failed to initialize ADS Board #0.");
@@ -83,130 +103,108 @@ void setup()
             ;
     }
 
-    // initialize the values into the array
-    for (int i = 0; i < 16; i++)
-    {
-        readADCVal(i, adcVals);
-    }
-}
-
-void readADCVal(int sensorNumber, double *sensorVals)
-{
-    int16_t adc;
-    double volts;
-
-    if (sensorNumber < 4)
-    {
-        adc = ads0.readADC_SingleEnded(sensorNumber);
-        volts = ads0.computeVolts(adc);
-    }
-    else if (sensorNumber > 3 && sensorNumber < 8)
-    {
-        adc = ads1.readADC_SingleEnded(sensorNumber - 4);
-        volts = ads1.computeVolts(adc);
-    }
-    else if (sensorNumber > 7 && sensorNumber < 12)
-    {
-        adc = ads2.readADC_SingleEnded(sensorNumber - 8);
-        volts = ads2.computeVolts(adc);
-    }
-    else
-    {
-        adc = ads3.readADC_SingleEnded(sensorNumber - 12);
-        volts = ads3.computeVolts(adc);
-    }
-    sensorVals[sensorNumber] = volts;
+    // start UDP
+    Udp.begin(localPort);
 }
 
 void loop()
 {
-
-    unsigned long time = millis();
-    unsigned long deltaTime;
-
-    // for (int i = 0; i < 16; i++)
-    // {
-    //     readADCVal(i, adcVals);
-    // }
-
-        deltaTime = millis() - time;
-    Serial.print("Time taken to record was:");
-    Serial.println(deltaTime);
-
-    time = millis();
-
-    // listen for incoming clients
-    EthernetClient client = server.available();
-
-    if (client)
+    /* if there's data available, read a packet */
+    int packetSize = Udp.parsePacket();
+    if (packetSize)
     {
-        Serial.println("new client");
-        // an http request ends with a blank line
-        boolean currentLineIsBlank = true;
-        while (client.connected())
+        Serial.print("Received packet of size ");
+        Serial.println(packetSize);
+        Serial.print("From ");
+        IPAddress remote = Udp.remoteIP();
+        for (int i = 0; i < 4; i++)
         {
-            if (client.available())
+            Serial.print(remote[i], DEC);
+            if (i < 3)
             {
-                char c = client.read();
-                // Serial.write(c);
-                //  if you've gotten to the end of the line (received a newline
-                //  character) and the line is blank, the http request has ended,
-                //  so you can send a reply
-                if (c == '\n' && currentLineIsBlank)
-                {
-                    // send a standard http response header
-                    client.println("HTTP/1.1 200 OK");
-                    client.println("Content-Type: application/json");
-                    client.println("Connection: close"); // the connection will be closed after completion of the response
-                    client.println("Refresh: 1");        // refresh the page automatically every 1 sec
-                    client.println();
-                    client.print("[");
-                    for (int i = 0; i < 16; i++)
-                    {
-                        client.print("{\"id\":");
-                        client.print(i);
-                        client.print(",\"value\":");
-                        client.print(adcVals[i]);
-                        if (i == 15)
-                        {
-                            client.print("}");
-                        }
-                        else
-                        {
-                            client.print("},");
-                        }
-                    }
-                    client.println("]");
-                    break;
-                }
-                if (c == '\n')
-                {
-                    // you're starting a new line
-                    currentLineIsBlank = true;
-                }
-                else if (c != '\r')
-                {
-                    // you've gotten a character on the current line
-                    currentLineIsBlank = false;
-                }
+                Serial.print(".");
             }
         }
-        // give the web browser time to receive the data
-        delay(1);
-        // close the connection:
-        client.stop();
-        Serial.println("client disconnected");
+        Serial.print(", port ");
+        Serial.println(Udp.remotePort());
 
-        deltaTime = millis() - time;
-        Serial.print("Time taken to print was:");
-        Serial.println(deltaTime);
-    }
-    else
-    { // there is not a request from the client for sensor information, record the values
-        for (int i = 0; i < 16; i++)
+        /* read the packet into packetBufffer */
+        Udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+        Serial.println("Contents:");
+        Serial.println(packetBuffer);
+
+        if (packetBuffer[3] != ':')
         {
-            readADCVal(i, adcVals);
-            // time quantum for recording all the adc values and storing them on an array is at least 160ms
+            Udp.beginPacket(Udp.remoteIP(), 5555);
+            Udp.write("ERR:invald_cmd_format");
+            Udp.endPacket();
+        }
+        else
+        {
+            if (packetBuffer[0] == 'S' && packetBuffer[1] == 'A' && packetBuffer[2] == 'B') // Enable or Disable Autobroadcasting
+            {
+
+                if (commandValValid(0, 1, packetBuffer))
+                {
+                    autoBroadcasting = getCommandVaue(packetBuffer);
+                    sendUPDString(packetBuffer);
+                }
+                else
+                {
+                    sendUPDString("ERR:invalid_cmd_value");
+                }
+            }
+            else if (packetBuffer[0] == 'S' && packetBuffer[1] == 'T' && packetBuffer[2] == 'L') // Set Num Transition Levels
+            {
+                if (commandValValid(1, 100, packetBuffer))
+                {
+                    transitionLevels = getCommandVaue(packetBuffer);
+                    sendUPDString(packetBuffer);
+                }
+                else
+                {
+                    sendUPDString("ERR:invalid_cmd_value");
+                }
+            }
+            else if (packetBuffer[0] == 'V' && packetBuffer[1] == 'A' && packetBuffer[2] == 'L') // Get Transition level value
+            {
+
+                int16_t adc;
+                double volts;
+
+                adc = ads0.readADC_SingleEnded(0);
+                volts = ads0.computeVolts(adc);
+                double currentLevel = ceil((double)(volts * transitionLevels) / 5);
+                char cmdOut[6];
+                sprintf(cmdOut, "VAL:%d", (int)currentLevel);
+                sendUPDString(cmdOut);
+            }
+            else
+            {
+                sendUPDString("ERR:invalid_cmd");
+            }
         }
     }
+
+    if (autoBroadcasting)
+    {
+
+        int16_t adc;
+        double volts;
+
+        adc = ads0.readADC_SingleEnded(0);
+        volts = ads0.computeVolts(adc);
+
+        double currentLevel = ceil((double)(volts * transitionLevels) / 5);
+        if (ceil((double)(adcLevel * transitionLevels) / 5) != currentLevel)
+        {
+            adcLevel = volts;
+
+            char cmdOut[6];
+            sprintf(cmdOut, "VAL:%d", (int)currentLevel);
+
+            sendUPDString(cmdOut);
+        }
+    }
+    delay(10);
 }
